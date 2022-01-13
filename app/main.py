553bf2f8beceece00c8024fba298429b08f2b875
roland813 <olegkorostensky@gmail.com>
2022-01-13 12:26:09 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -2,17 +2,18 @@ def create_dict(*args):
     diction = {}
     value = 0
     for argument in args:
-    #for argument in enumerate(args) not need value in line4 and value to dict will be index
-        if isinstance(argument, int) or isinstance(argument, float)\
-                or isinstance(argument, str) or isinstance(argument, bool)\
+        # for argument in enumerate(args)
+        # not need value in line4 and value to dict will be index
+        if isinstance(argument, int) or isinstance(argument, float) \
+                or isinstance(argument, str) or isinstance(argument, bool) \
                 or argument is None or callable(argument):
-        # if isinstance(argument, (int, float, str, bool))
+            # if isinstance(argument, (int, float, str, bool))
             diction[argument] = value
             value += 1
         elif isinstance(argument, tuple):
             statment = 0
             for arg in argument:
-                if isinstance(arg, list) or isinstance(arg, set)\
+                if isinstance(arg, list) or isinstance(arg, set) \
                         or isinstance(arg, dict):
                     statment += 1
             if statment == 0:
